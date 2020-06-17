2eb7790020354a36394257ca3cb2611c3ccd31be
xuhua <cxuhua@gmail.com>
2020-06-17 16:21:12 +0800

    save

--- core/redisimp.go
+++ core/redisimp.go
@@ -6,8 +6,55 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
+	"github.com/bsm/redislock"
 	"github.com/go-redis/redis/v7"
 )
+
+//ILocker 缓存锁，需要基于分布式锁实现
+type ILocker interface {
+	//Release 释放锁
+	Release()
+	//TTL 锁超时时间 返回0表示锁已经释放
+	TTL() (time.Duration, error)
+	//Refresh 更新锁超时时间
+	Refresh(ttl time.Duration) error
+	//获取meta数据
+	Metadata() string
+}
+
+type redislocker struct {
+	l *redislock.Lock
+}
+
+func (d *redislocker) Release() {
+	d.l.Release()
+}
+
+func (d *redislocker) Metadata() string {
+	return d.l.Metadata()
+}
+
+func (d *redislocker) TTL() (time.Duration, error) {
+	return d.l.TTL()
+}
+
+func (d *redislocker) Refresh(ttl time.Duration) error {
+	return d.l.Refresh(ttl, nil)
+}
+
+//NewRedisLocker 创建基于redis得分布式锁
+func NewRedisLocker(c redislock.RedisClient, key string, ttl time.Duration, meta ...string) (ILocker, error) {
+	opts := &redislock.Options{}
+	if len(meta) > 0 {
+		opts.Metadata = meta[0]
+	}
+	l := redislock.New(c)
+	lp, err := l.Obtain(key, ttl, opts)
+	if err != nil {
+		return nil, err
+	}
+	return &redislocker{l: lp}, nil
+}
 
 //IRedisImp redis接口
 type IRedisImp interface {
@@ -21,12 +68,18 @@ type IRedisImp interface {
 	Subscribe(channels ...string) *redis.PubSub
 	//发布消息
 	Publish(channel string, message interface{}) error
+	//分布式锁实现
+	Locker(key string, ttl time.Duration, meta ...string) (ILocker, error)
 }
 
 type redisImp struct {
 	context.Context
 	rcli *redis.Client
 	conn *redis.Conn
+}
+
+func (rimp *redisImp) Locker(key string, ttl time.Duration, meta ...string) (ILocker, error) {
+	return NewRedisLocker(rimp.conn, key, ttl, meta...)
 }
 
 func (rimp *redisImp) DelUserID(k string) error {
@@ -39,6 +92,7 @@ func (rimp *redisImp) Subscribe(channels ...string) *redis.PubSub {
 	return rimp.rcli.Subscribe(channels...)
 }
 
+//Publish 发布消息
 func (rimp *redisImp) Publish(channel string, message interface{}) error {
 	return rimp.conn.Publish(channel, message).Err()
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ replace github.com/cxuhua/xginx v0.0.1 => ../xginx
 replace github.com/cxuhua/gopher-lua v1.0.1 => ../gopher-lua
 
 require (
+	github.com/bsm/redislock v0.5.0
 	github.com/cxuhua/xginx v0.0.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/form/v4 v4.1.1
